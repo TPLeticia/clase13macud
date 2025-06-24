3132eab5eff16a0beeb25e32a8418c991c525250
Leticia Tonacatl Perez <leticiatonacatlperez@MacBook-Air-de-Leticia.local>
2025-06-24 16:13:19 -0600

    servidor funcionando, borre .gitignore de src

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.get('/', (req,res)=> {
+    res.send('Hello Compañeros!');
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor a la escucha en http://localhost:${PORT}`);
+    //console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
